6399b6c1b2168e36ee1c7c08de39a45952238888
JoblessSlime <raphael.thierry@e-artsup.net>
2025-12-01 11:07:54 +0100

    fix prefab

--- Assets/Script/UI/UI_InputManager.cs
+++ Assets/Script/UI/UI_InputManager.cs
@@ -4,17 +4,11 @@ using UnityEngine.UI;
 
 public class UI_InputManager : MonoBehaviour
 {
-    private EventSystem eventSystem;
     [SerializeField] private GameObject firstButton;
 
-    void OnEnable()
+    void Start()
     {
         EventSystem.current.SetSelectedGameObject(firstButton);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
