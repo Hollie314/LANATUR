1baaa191d2d79b3cae13b351a92a417aa837586b
JoblessSlime <raphael.thierry@e-artsup.net>
2025-12-30 12:00:51 +0100

    Auto stash before rebase of "Appareil_Photo" onto "origin/DEV"

--- Assets/Script/LookAtPlayer.cs
+++ Assets/Script/LookAtPlayer.cs
@@ -5,6 +5,7 @@ public class LookAtPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.LookAt(Camera.main.transform);
+        transform.LookAt(Camera.main.transform, new Vector3(0,0,0));
+        transform.eulerAngles = new Vector3(0,transform.eulerAngles.y,0);
     }
 }
