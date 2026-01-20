5d42cd629589239e28a9c6362b8b978f5a07ebda
Hollie314 <157151660+Hollie314@users.noreply.github.com>
2026-01-20 11:40:51 +0100

    light son et trigger

---
+++ Assets/Script/Story Events/Trigger_Event.cs
@@ -0,0 +1,26 @@
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Trigger_Event : MonoBehaviour
+{
+    [SerializeField] bool destroyOnTriggerEnter;
+    [SerializeField] string tagFilter;
+    [SerializeField] UnityEvent onTriggerEnter;
+    [SerializeField] UnityEvent onTriggerExit;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (!String.IsNullOrEmpty(tagFilter)&& !other.gameObject.CompareTag(tagFilter)) return;
+        onTriggerEnter.Invoke();
+        if (destroyOnTriggerEnter)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OntiggerExit(Collider other)
+    {
+        onTriggerExit.Invoke();
+    }
+}
