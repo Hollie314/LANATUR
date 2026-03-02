2e05f946d604e98abe4bc9056b86ae2464427290
IvyDrag0n <sandyblackeco@gmail.com>
2026-03-02 22:29:41 +0100

    portfolio du ku

--- Assets/Project/Material/Test_Amanda/Screenshots.cs
+++ Assets/Project/Material/Test_Amanda/Screenshots.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class Screenshots : MonoBehaviour
 {
-    void TakeScreenshot()
+    public void TakeScreenshot()
     {
         ScreenCapture.CaptureScreenshot("portfolio.png");
     }
