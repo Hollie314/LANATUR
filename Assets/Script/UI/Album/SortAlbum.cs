e8a15bf6d201b615d76b86d5b432b95f34e9ce9c
JoblessSlime <raphael.thierry@e-artsup.net>
2026-03-04 13:10:09 +0100

    Commit before changing photosinfos

--- Assets/Script/UI/Album/SortAlbum.cs
+++ Assets/Script/UI/Album/SortAlbum.cs
@@ -1,6 +1,6 @@
 using System.Collections.Generic;
 using UnityEngine;
-using System.Collections.Generic;
+using TMPro;
 using Sirenix.OdinInspector;
 
 public class SortAlbum : MonoBehaviour
@@ -34,10 +34,17 @@ public class SortAlbum : MonoBehaviour
     [Header("UI Elements")]
     [SerializeField] private GameObject ScrollVerticalLayout;
     [SerializeField] private GameObject PhotoPrefab;
+    [SerializeField] private TMP_Dropdown DropdownSize;
+    [SerializeField] private TMP_Dropdown DropdownSortType1;
+    [SerializeField] private TMP_Dropdown DropdownSortType2;
     
     private List<PhotoInfos> AlbumSortedType1;
     private List<PhotoInfos> AlbumSortedType2;
     private Dictionary<PhotoInfos, GameObject> albumDictionary = new Dictionary<PhotoInfos, GameObject>();
+
+    private GameObject scroll1 = new GameObject();
+    private GameObject scroll2 = new GameObject();
+    private GameObject scroll3 = new GameObject();
     
     // When Album is Enabled, reset it in case new photos appeared
     void OnEnable()
@@ -66,40 +73,98 @@ public class SortAlbum : MonoBehaviour
     }
 
     #region SortType1
-    public void SortAll()
+    public void Sort1()
+    {
+        switch (DropdownSortType1.value)
+        {
+            case 0:
+                SortAll();
+                break;
+            case 1:
+                SortFav();
+                break;
+            case 2:
+                SortSpecies();
+                break;
+            case 3:
+                SortEncyclopedia();
+                break;
+            case 4:
+                SortRen();
+                break;
+            case 5:
+                SortCorpo();
+                break;
+        }
+    }
+    
+    private void SortAll()
     {
         sortType1 = SortType1.All;
-        
+        Sort2(false);
     }
     
-    public void SortFav()
+    private void SortFav()
     {
         sortType1 = SortType1.Fav;
+        Sort2(false);
     }
 
-    public void SortSpecies()
+    private void SortSpecies()
     {
         sortType1 = SortType1.Species;
+        Sort2(false);
     }
     
-    public void SortEncyclopedia()
+    private void SortEncyclopedia()
     {
         sortType1 = SortType1.Encyclopedia;
+        Sort2(false);
     }
     
-    public void SortRen()
+    private void SortRen()
     {
         sortType1 = SortType1.Ren;
+        Sort2(false);
     }
     
-    public void SortCorpo()
+    private void SortCorpo()
     {
         sortType1 = SortType1.Corporation;
+        Sort2(false);
     }
     #endregion
     
     #region SortType2
-    public void SortByDate_OldestFirst()
+    public void Sort2(bool calledInUI)
+    {
+        if (calledInUI)
+        {
+            switch (DropdownSortType2.value)
+            {
+                case 0:
+                    SortByDate_OldestFirst();
+                    break;
+                case 1:
+                    SortByDate_NewestFirst();
+                    break;
+            }
+        }
+        else
+        {
+            switch (sortType2)
+            {
+                case SortType2.OldestFirst:
+                    SortByDate_OldestFirst();
+                    break;
+                case SortType2.NewestFirst:
+                    SortByDate_NewestFirst();
+                    break;
+            }
+        }
+    }
+    
+    private void SortByDate_OldestFirst()
     {
         sortType2 = SortType2.OldestFirst;
         
@@ -138,7 +203,7 @@ public class SortAlbum : MonoBehaviour
         ShowPhotos();
     }
 
-    public void SortByDate_NewestFirst()
+    private void SortByDate_NewestFirst()
     {
         sortType2 = SortType2.NewestFirst;
         foreach (PhotoInfos photoInfo in AlbumSortedType1)
@@ -149,20 +214,90 @@ public class SortAlbum : MonoBehaviour
     }
     #endregion
 
+    #region AlbumSize
+    public void SortAlbumSize()
+    {
+        switch (DropdownSortType2.value)
+        {
+            case 0:
+                SizeSmall();
+                break;
+            case 1:
+                SizeMedium();
+                break;
+            case 2:
+                SizeLarge();
+                break;
+        }
+    }
+    
+    private void SizeSmall()
+    {
+        albumSize = AlbumSize.Small;
+        DestroyScrollview();
+        
+        //Utiliser un seul scroll
+        scroll1 = ScrollVerticalLayout.transform.GetChild(0).gameObject;
+        
+        ShowPhotos();
+    }
+
+    private void SizeMedium()
+    {
+        albumSize = AlbumSize.Small;
+        DestroyScrollview();
+        
+        //Utiliser 2 scrolls
+        scroll1 = ScrollVerticalLayout.transform.GetChild(0).gameObject;
+        scroll2 = Instantiate(new GameObject(), ScrollVerticalLayout.transform);
+        
+        ShowPhotos();
+    }
+
+    private void SizeLarge()
+    {
+        albumSize = AlbumSize.Small;
+        DestroyScrollview();
+        
+        //Utiliser 3 scrolls
+        scroll1 = ScrollVerticalLayout.transform.GetChild(0).gameObject;
+        scroll2 = Instantiate(new GameObject(), ScrollVerticalLayout.transform);
+        scroll3 = Instantiate(new GameObject(), ScrollVerticalLayout.transform);
+        
+        ShowPhotos();
+    }
+    
+    private void DestroyScrollview()
+    {
+        for (int i = 1; i < ScrollVerticalLayout.transform.childCount; i++)
+        {
+            Destroy(ScrollVerticalLayout.transform.GetChild(i).gameObject);
+        }
+    }
+    #endregion
+
     #region ShowPhotos
     private void ShowPhotos()
     {
-        switch (albumSize)
+        int index = 0;
+        foreach (PhotoInfos photoInfo in AlbumSortedType2)
         {
-            case AlbumSize.Small:
-                ShowSmall();
-                break;
-            case AlbumSize.Medium:
-                ShowMedium();
-                break;
-            case AlbumSize.Large:
-                ShowLarge();
-                break;
+            // Vérifier si la photo existe déjà en mémoire
+            GameObject photo = new GameObject();
+            if (albumDictionary.ContainsKey(photoInfo)) { photo = albumDictionary[photoInfo]; }
+            else
+            {
+                photo = CreatePhoto(photoInfo);
+                albumDictionary.Add(photoInfo, photo);
+            }
+            
+            if (index % 3 == 0 && (albumSize == AlbumSize.Large))
+                Instantiate(photo, scroll3.transform);
+            else if (index % 2 == 0 && (albumSize == AlbumSize.Large || albumSize == AlbumSize.Medium))
+                Instantiate(photo, scroll2.transform);
+            else
+                Instantiate(photo, scroll1.transform);
+            index++;
         }
     }
 
@@ -171,76 +306,6 @@ public class SortAlbum : MonoBehaviour
         GameObject photo = PhotoPrefab;
         if(photoInfo.imageUsedInEncyclopedia){}
         return photo;
-    }
-
-    private void ShowSmall()
-    {
-        //Utiliser un seul scroll
-        foreach (PhotoInfos photoInfo in AlbumSortedType2)
-        {
-            // Vérifier si la photo existe déjà en mémoire
-            GameObject photo = new GameObject();
-            if (albumDictionary.ContainsKey(photoInfo)) { photo = albumDictionary[photoInfo]; }
-            else
-            {
-                photo = CreatePhoto(photoInfo);
-                albumDictionary.Add(photoInfo, photo);
-            }
-            
-            Instantiate(photo);
-        }
-    }
-
-    private void ShowMedium()
-    {
-        //Utiliser 2 scrolls
-        int index = 0;
-        foreach (PhotoInfos photoInfo in AlbumSortedType2)
-        {
-            // Vérifier si la photo existe déjà en mémoire
-            GameObject photo = new GameObject();
-            if (albumDictionary.ContainsKey(photoInfo)) { photo = albumDictionary[photoInfo]; }
-            else
-            {
-                photo = CreatePhoto(photoInfo);
-                albumDictionary.Add(photoInfo, photo);
-            }
-            
-            if (index % 2 == 0)
-            {
-                
-            }
-            else{}
-            index++;
-        }
-    }
-
-    private void ShowLarge()
-    {
-        //Utiliser 3 scrolls
-        int index = 0;
-        foreach (PhotoInfos photoInfo in AlbumSortedType2)
-        {
-            // Vérifier si la photo existe déjà en mémoire
-            GameObject photo = new GameObject();
-            if (albumDictionary.ContainsKey(photoInfo)) { photo = albumDictionary[photoInfo]; }
-            else
-            {
-                photo = CreatePhoto(photoInfo);
-                albumDictionary.Add(photoInfo, photo);
-            }
-            
-            if (index % 3 == 0)
-            {
-                
-            }
-            else if (index % 2 == 0)
-            {
-                
-            }
-            else{}
-            index++;
-        }
     }
     #endregion
 }
