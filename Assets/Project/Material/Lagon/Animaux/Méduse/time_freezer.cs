b03ef0bb70dcf4d31d221cf03345623e22bbd1e3
IvyDrag0n <sandyblackeco@gmail.com>
2026-03-11 12:08:36 +0100

    kill me pls

---
+++ Assets/Project/Material/Lagon/Animaux/Méduse/time_freezer.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+//UNITY_SHADER_NO_UPGRADE
+#ifndef MYHLSLINCLUDE_INCLUDED
+#define MYHLSLINCLUDE_INCLUDED
+float4x4 _MyMatrix;
+void MyFunction_float(float Time, float firstTime = 0, float Out)
+{
+    
+}
+#endif //MYHLSLINCLUDE_INCLUDED
+
