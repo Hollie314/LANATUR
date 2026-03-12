d937933b73cd22c2e14a9672d6fa812a6f01810b
IvyDrag0n <sandyblackeco@gmail.com>
2026-03-12 10:01:05 +0100

    les bubulles.... mais pas de vfx

--- Assets/Project/Material/Lagon/Animaux/Méduse/time_freezer.cs
+++ Assets/Project/Material/Lagon/Animaux/Méduse/time_freezer.cs
@@ -1,12 +1,18 @@
+using System;
 using UnityEngine;
 
 //UNITY_SHADER_NO_UPGRADE
 #ifndef MYHLSLINCLUDE_INCLUDED
 #define MYHLSLINCLUDE_INCLUDED
-float4x4 _MyMatrix;
+
 void MyFunction_float(float Time, float firstTime = 0, float Out)
 {
-    
+    if (firstTime == 0)
+    {
+        firstTime = Time;
+    }
+
+    Out = firstTime;
 }
 #endif //MYHLSLINCLUDE_INCLUDED
 
