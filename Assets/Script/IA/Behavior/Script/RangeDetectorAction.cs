e22b85f95342129623c73ae5c6150863dd63ee04
Hollie314 <157151660+Hollie314@users.noreply.github.com>
2026-03-16 21:31:13 +0100

    conflit de node mais normalement à regler

--- Assets/Script/IA/Behavior/Script/RangeDetectorAction.cs
+++ Assets/Script/IA/Behavior/Script/RangeDetectorAction.cs
@@ -5,17 +5,16 @@ using Action = Unity.Behavior.Action;
 using Unity.Properties;
 
 [Serializable, GeneratePropertyBag]
-[NodeDescription(name: "RangeDetector", story: "Update Range [Detector] assign [Target] check [Layer]", category: "Action", id: "d024bc68f12566b4b483a6e2b2a03bc3")]
+[NodeDescription(name: "detection", story: "Check for closest target in layer", category: "Action", id: "d024bc68f12566b4b483a6e2b2a03bc3")]
 public partial class RangeDetectorAction : Action
 {
     [SerializeReference] public BlackboardVariable<RangeDetection> Detector;
-    [SerializeReference] public BlackboardVariable<GameObject> Target;
     [SerializeReference] public BlackboardVariable<int> Layer;
 
     protected override Status OnUpdate()
     {
-        Target.Value = Detector.Value.UpdateDetector(Layer.Value);
-        return Detector.Value.UpdateDetector(Layer.Value) == null ? Status.Failure : Status.Success;
+        // Just check if there is any object in the layer
+        GameObject detected = Detector.Value.UpdateDetector(Layer.Value);
+        return detected == null ? Status.Failure : Status.Success;
     }
 }
-
