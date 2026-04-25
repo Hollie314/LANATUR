70c8b64d8518a9c28bf1fe7177ca8fde26d5954e
JoblessSlime <raphael.thierry@e-artsup.net>
2026-04-25 21:24:43 +0200

    Sensi solution trouvée

--- Assets/Script/Player/PlayerLook.cs
+++ Assets/Script/Player/PlayerLook.cs
@@ -1,5 +1,6 @@
 using Unity.Cinemachine;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerLook : MonoBehaviour
 
@@ -9,8 +10,9 @@ public class PlayerLook : MonoBehaviour
     private float yRotation = 0f;
     private bool clampLeftRight = false;
     
-    public float xSensitivity = 30f;
-    public float ySensitivity = 30f;
+    [SerializeField] private Slider SensitivitySlider; 
+    public float xSensitivity = 3;
+    public float ySensitivity = 3f;
 
     public void ProcessLook(Vector2 input)
     {
@@ -34,6 +36,15 @@ public class PlayerLook : MonoBehaviour
         {
             cam.transform.localRotation = Quaternion.Euler(yRotation, 0f, 0f);
             transform.Rotate(Vector3.up * (mouseX * Time.deltaTime) * xSensitivity);
+        }
+    }
+
+    public void OnValueChanged()
+    {
+        if (SensitivitySlider != null)
+        {
+            xSensitivity = SensitivitySlider.value;
+            ySensitivity = SensitivitySlider.value;   
         }
     }
 
