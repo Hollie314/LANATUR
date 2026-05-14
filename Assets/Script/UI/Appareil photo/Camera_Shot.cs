98c754ad608f84f4bb689f86f4d2d013fc188e56
Hollie314 <157151660+Hollie314@users.noreply.github.com>
2026-05-14 16:36:56 +0200

    bug reparer

--- Assets/Script/UI/Appareil photo/Camera_Shot.cs
+++ Assets/Script/UI/Appareil photo/Camera_Shot.cs
@@ -92,14 +92,13 @@ public class Camera_Shot : MonoBehaviour
 
     private void Start()
     {
-        screenCapture = new Texture2D(Screen.width, Screen.height, DefaultFormat.HDR, TextureCreationFlags.None); // Change dimensions
+        screenCapture = new Texture2D(Screen.width, Screen.height, DefaultFormat.HDR, TextureCreationFlags.None);
     }
     
     public List<GameObject> GetVisibleObjects()
     {
         List<GameObject> visibleObjects = new List<GameObject>();
 
-        // Get all renderers in the scene
         Renderer[] renderers = FindObjectsOfType<Renderer>();
 
         Plane[] planes = GeometryUtility.CalculateFrustumPlanes(Camera.main);
@@ -115,42 +114,51 @@ public class Camera_Shot : MonoBehaviour
             // Check if inside camera frustum
             if (GeometryUtility.TestPlanesAABB(planes, rend.bounds))
             {
-                // Debug.Log($"CAMERASHOT {obj.name} is visible part -2");
-                if (obj.TryGetComponent<UpdateEntry>(out UpdateEntry animalPart) || obj.transform.parent.TryGetComponent<UpdateEntry>(out UpdateEntry updatePart))
+                // FIX NULL PARENT
+                if (
+                    obj.TryGetComponent<UpdateEntry>(out UpdateEntry animalPart) ||
+                    (
+                        obj.transform.parent != null &&
+                        obj.transform.parent.TryGetComponent<UpdateEntry>(out UpdateEntry updatePart)
+                    )
+                )
                 {
-                    //how far you can check a point
-                    
                     Vector2 screenCenter = new Vector2(Screen.width / 2f, Screen.height / 2f);
                     Vector3 screenPos = Camera.main.WorldToScreenPoint(obj.transform.position);
-                    // Make a 2D vector (ignore Z)
+
                     Vector2 screenPos2D = new Vector2(screenPos.x, screenPos.y);
-                    // Calculate 2D distance from center
+
                     float distance = Vector2.Distance(screenCenter, screenPos2D);
-                    // Debug.Log($"CAMERASHOT {obj.name} is visible part -1, distance: {distance}");
+
                     if (distance > distanceToSeePoint)
                         continue;
                     
-                    // Debug.Log($"CAMERASHOT {obj.name} is visible part 0");
                     if (planes.All(plane => plane.GetDistanceToPoint(obj.transform.position) >= 0))
                     {
-                        // Debug.Log($"CAMERASHOT {obj.name} is visible part 1");
                         Vector3 cameraPos = Camera.main.transform.position;
                         Vector3 direction = (obj.transform.position - cameraPos).normalized;
 
                         if (Physics.Raycast(cameraPos, direction, out RaycastHit hit))
                         {
-                            // Debug.Log($"CAMERASHOT {obj.name} is visible part 2, hit.name: {hit.collider.name}");
-                            if (hit.collider.gameObject == obj || hit.collider.gameObject == obj.transform.parent.gameObject)
+                            // FIX NULL PARENT
+                            if (
+                                hit.collider.gameObject == obj ||
+                                (
+                                    obj.transform.parent != null &&
+                                    hit.collider.gameObject == obj.transform.parent.gameObject
+                                )
+                            )
                             {
-                                // Debug.Log($"CAMERASHOT {obj.name} is visible part 3");
-                                
-                                // Fin
-                                if (! obj.TryGetComponent<UpdateEntry>(out UpdateEntry onSenBranle))
+                                if (!obj.TryGetComponent<UpdateEntry>(out UpdateEntry onSenBranle))
                                 {
-                                    obj = obj.transform.parent.gameObject;
+                                    // FIX NULL PARENT
+                                    if (obj.transform.parent != null)
+                                    {
+                                        obj = obj.transform.parent.gameObject;
+                                    }
                                 }
+
                                 visibleObjects.Add(obj);
-                                // Debug.Log($"CAMERASHOT is visible part 4 added {obj.name}");
                             }
                         }
                     }
@@ -163,9 +171,8 @@ public class Camera_Shot : MonoBehaviour
 
     private void Update()
     {
-        //Debug.Log($"Can change camera: {canChangeCameraUI}, OpenUI: {_openUI.canChangeCameraUI}");
-        
         timeSinceLastPicture += Time.deltaTime;
+
         if (!canChangeCameraUI && timeSinceLastPicture >= timeBeforeClosingUI)
         {
             canChangeCameraUI = true;
@@ -173,6 +180,7 @@ public class Camera_Shot : MonoBehaviour
         }
 
         timeSinceLastCheckVisible += Time.deltaTime;
+
         if (timeSinceLastCheckVisible >= TimeToCheckVisible)
         {
             timeSinceLastCheckVisible = 0f;
@@ -181,10 +189,11 @@ public class Camera_Shot : MonoBehaviour
         
         if (!interestPointsVisible.Contains(target))
             target = null;
+
         CameraDetection();
-        if (Input.GetMouseButtonDown(0))     // Use new input system ---------------------------------------------------------------------------------------------------
+
+        if (Input.GetMouseButtonDown(0))
         {
-            //Caper les interactions
             if (timeSinceLastPicture >= timeBetweenPictures)
             {
                 timeSinceLastPicture = 0f;
@@ -200,10 +209,11 @@ public class Camera_Shot : MonoBehaviour
         {
             if(!lockTarget.activeSelf)
                 lockTarget.SetActive(true);
-            // Convert object world position to screen space
+
             Vector3 screenPos = Camera.main.WorldToScreenPoint(target.transform.position);
-            // Make a 2D vector (ignore Z)
+
             Vector2 screenPos2D = new Vector2(screenPos.x, screenPos.y);
+
             lockTarget.transform.position = screenPos2D;
         }
         else
@@ -212,25 +222,21 @@ public class Camera_Shot : MonoBehaviour
                 lockTarget.SetActive(false);
         }
 
-        // ZoomCursor
         Vector2 screenCenter = new Vector2(Screen.width / 2f, Screen.height / 2f);
         float closestDistance = Mathf.Infinity;
+
         if (!interestPointsVisible.IsNullOrEmpty())
         {
             foreach (GameObject interestPoint in interestPointsVisible)
             {
-                //Debug.Log($"added list length; {interestPointsVisible.Count}");
-                    //Debug.Log($"added doing {interestPoint.gameObject.name}");
-                    // Convert object world position to screen space
-                    Vector3 screenPos = Camera.main.WorldToScreenPoint(interestPoint.transform.position);
+                Vector3 screenPos = Camera.main.WorldToScreenPoint(interestPoint.transform.position);
 
-                    // Make a 2D vector (ignore Z)
-                    Vector2 screenPos2D = new Vector2(screenPos.x, screenPos.y);
+                Vector2 screenPos2D = new Vector2(screenPos.x, screenPos.y);
 
-                    // Calculate 2D distance from center
-                    float distance = Vector2.Distance(screenCenter, screenPos2D);
-                    if (distance < closestDistance)
-                        closestDistance = distance;
+                float distance = Vector2.Distance(screenCenter, screenPos2D);
+
+                if (distance < closestDistance)
+                    closestDistance = distance;
             }
         }
 
@@ -243,7 +249,7 @@ public class Camera_Shot : MonoBehaviour
         float percentage = (closestDistance / maxDistanceToZoom);
         float zoom = Mathf.Lerp(cursorSizeOnTarget, cursorSizeOnNothing, percentage);
         Color color = Color.Lerp(colorOnTarget, colorOnNothing, percentage);
-        //Debug.Log($"closest distance: {closestDistance}, percentage: {percentage}, Zoom: {zoom}, cursorSizeOnTarget: {cursorSizeOnTarget}, cursorSizeOnNothing: {cursorSizeOnNothing}");
+
         SetCursorSizeAndColor(zoom, color);
     }
     
@@ -259,32 +265,33 @@ public class Camera_Shot : MonoBehaviour
             return;
         
         RaycastHit hitInfo;
-        ////Debug.Log(Camera.main.farClipPlane);
+
         if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.TransformDirection(Vector3.forward), out hitInfo, Camera.main.farClipPlane, animals_LayerMask))
         {
-            // Debug.Log($"CAMERASHOT is visible part 5 {hitInfo.collider.name} {Time.fixedTime}");
-                if (target != hitInfo.collider.gameObject)
+            if (target != hitInfo.collider.gameObject)
+            {
+                if (interestPointsVisible.Contains(hitInfo.collider.gameObject))
                 {
-                    // Debug.Log($"CAMERASHOT is visible part 6 {hitInfo.collider.name} {Time.fixedTime}");
-                    if (interestPointsVisible.Contains(hitInfo.collider.gameObject))
-                    {
-                        target = hitInfo.collider.gameObject;
-                        // Debug.Log($"CAMERASHOT is visible part 7 {target.name} {Time.fixedTime}");
+                    target = hitInfo.collider.gameObject;
                 
-                        audioSource_Photo.clip = SFX_TargetLocked;
-                        audioSource_Photo.Play();
-                        return;
-                    }
-                    else if (interestPointsVisible.Contains(hitInfo.collider.gameObject.transform.parent.gameObject))
-                    {
-                        target = hitInfo.collider.gameObject;
-                        // Debug.Log($"CAMERASHOT is visible part 7 {target.name} {Time.fixedTime}");
-                
-                        audioSource_Photo.clip = SFX_TargetLocked;
-                        audioSource_Photo.Play();
-                        return;
-                    }
+                    audioSource_Photo.clip = SFX_TargetLocked;
+                    audioSource_Photo.Play();
+                    return;
                 }
+
+                // FIX NULL PARENT
+                else if (
+                    hitInfo.collider.transform.parent != null &&
+                    interestPointsVisible.Contains(hitInfo.collider.transform.parent.gameObject)
+                )
+                {
+                    target = hitInfo.collider.gameObject;
+                
+                    audioSource_Photo.clip = SFX_TargetLocked;
+                    audioSource_Photo.Play();
+                    return;
+                }
+            }
         }
     }
 
@@ -294,20 +301,25 @@ public class Camera_Shot : MonoBehaviour
     }
 
     #region TakePhoto
+
     IEnumerator TakePicture()
     {
         yield return new WaitForEndOfFrame();
+
         audioSource_Photo.clip = SFX_TakePhoto;
         audioSource_Photo.Play();
+
         CapturePhoto();
+
         Camera_UI.UpdateAlbumPicture(screenCapture);
     }
 
     private void CapturePhoto()
     {
-        // Create Texture
         Camera.main.cullingMask = MaskCameraOnShot;
+
         RenderTexture rt = new RenderTexture(Screen.width, Screen.height, 24, DefaultFormat.HDR);
+
         Camera.main.targetTexture = rt;
 
         Camera.main.Render();
@@ -321,13 +333,14 @@ public class Camera_Shot : MonoBehaviour
         
         Camera.main.cullingMask = MaskCameraVisible;
 
-        // Album
         if (target != null)
         {
             album = Album.Load();
+
             if (!album.photoInfos.IsNullOrEmpty())
             {
                 bool isNewSpecies = true;
+
                 foreach (var info in album.photoInfos)
                 {
                     if(info.imageTag == target.tag)
@@ -335,28 +348,48 @@ public class Camera_Shot : MonoBehaviour
                         isNewSpecies = false;
                     }
                 }
+
                 if (isNewSpecies)
                 {
-                    PhotoInfos infos = SaveSystem.SavePicture(screenCapture, target.tag, true, target.GetComponent<UpdateEntry>().EncyclopedieEntry[0], target.GetComponent<UpdateEntry>().EntryType);
-                    if ( target.GetComponent<UpdateEntry>() != null)
+                    PhotoInfos infos = SaveSystem.SavePicture(
+                        screenCapture,
+                        target.tag,
+                        true,
+                        target.GetComponent<UpdateEntry>().EncyclopedieEntry[0],
+                        target.GetComponent<UpdateEntry>().EntryType
+                    );
+
+                    if (target.GetComponent<UpdateEntry>() != null)
                     {
                         target.GetComponent<UpdateEntry>().UpdateEntry_Func();
                         ChangeEntryPhoto.AddPhotoToEntries(infos);
                     }
                     
                     SpecieTakenInPhoto?.Invoke(target.tag);
-                    //Debug.Log("new species");
                 }
                 else
                 {
-                    SaveSystem.SavePicture(screenCapture, target.tag, false, null, target.GetComponent<UpdateEntry>().EntryType);
-                    //Debug.Log("not a new species");
+                    SaveSystem.SavePicture(
+                        screenCapture,
+                        target.tag,
+                        false,
+                        null,
+                        target.GetComponent<UpdateEntry>().EntryType
+                    );
+
                     SpecieTakenInPhoto?.Invoke(target.tag);
                 }
             }
             else
             {
-                PhotoInfos infos = SaveSystem.SavePicture(screenCapture, target.tag, true, target.GetComponent<UpdateEntry>().EncyclopedieEntry[0], target.GetComponent<UpdateEntry>().EntryType);
+                PhotoInfos infos = SaveSystem.SavePicture(
+                    screenCapture,
+                    target.tag,
+                    true,
+                    target.GetComponent<UpdateEntry>().EncyclopedieEntry[0],
+                    target.GetComponent<UpdateEntry>().EntryType
+                );
+
                 if (target.GetComponent<UpdateEntry>() != null)
                 {
                     target.GetComponent<UpdateEntry>().UpdateEntry_Func();
@@ -364,23 +397,34 @@ public class Camera_Shot : MonoBehaviour
                 }
                 
                 SpecieTakenInPhoto?.Invoke(target.tag);
-                //Debug.Log("no photos in album");
             }
         }
         else
         {
-            SaveSystem.SavePicture(screenCapture, null, false, null, PhotoInfos.ImageTypes.None);
-            //Debug.Log("no target");
+            SaveSystem.SavePicture(
+                screenCapture,
+                null,
+                false,
+                null,
+                PhotoInfos.ImageTypes.None
+            );
         }
         
-        Sprite photoSprite = Sprite.Create(screenCapture, new Rect(0.0f, 0.0f, screenCapture.width, screenCapture.height), new Vector2(0.5f, 0.5f), 100.0f);
+        Sprite photoSprite = Sprite.Create(
+            screenCapture,
+            new Rect(0.0f, 0.0f, screenCapture.width, screenCapture.height),
+            new Vector2(0.5f, 0.5f),
+            100.0f
+        );
+
         ShotAnim.GetComponent<Image>().sprite = photoSprite;
         ShotAnim.GetComponent<Animator>().Play("Photo_Flash");
 
-        // Reset all
         Camera.main.targetTexture = null;
         RenderTexture.active = null;
+
         Destroy(rt);
     }
+
     #endregion
 }
