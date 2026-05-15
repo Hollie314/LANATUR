6016e932dd5735475000abb39302eaf91cdfbc23
JoblessSlime <raphael.thierry@e-artsup.net>
2026-05-15 15:46:47 +0200

    Quest fix

--- Assets/Script/Quest/QuestPoint.cs
+++ Assets/Script/Quest/QuestPoint.cs
@@ -8,13 +8,18 @@ public class QuestPoint : MonoBehaviour
     [SerializeField] private bool updateQuest;
     private void OnTriggerEnter(Collider other)
     {
+        Debug.Log("quete trigger entré");
         if (other.tag == "Player")
         {
-            if (startsQuest)
+            Debug.Log("quete trigger entré tag player détecté");
+            if (startsQuest){
+                Debug.Log("quete trigger va start quest");
                 this.gameObject.GetComponent<StartQuestScript>().StartQuest();
-            
-            if (updateQuest)
+                Debug.Log("quete trigger a start quest");
+            }
+            if (updateQuest){
                 this.gameObject.GetComponent<UpdateQuest>().UpdateQuestProgress();
+            }
         }
     }
 }
